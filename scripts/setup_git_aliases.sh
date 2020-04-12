79082341f31cfa1161391f474610d628ccf90b5d
dchayes <dchayes12@gmail.com>
2020-04-12 12:23:53 -0500

    Add git oops alias

--- scripts/setup_git_aliases.sh
+++ scripts/setup_git_aliases.sh
@@ -2,4 +2,5 @@
 
 git config --global alias.co checkout
 git config --global alias.st status
+git config --global alias.oops "commit --amend --no-edit"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
