6aaba010772d6a3a226b6562560549e1eaf944e3
David <dchayes12@gmail.com>
2022-07-21 12:55:01 -0400

    add `pushup` alias
    Pushes, setting upstream to origin/<current branch>

--- scripts/setup_git_aliases.sh
+++ scripts/setup_git_aliases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 git config --global alias.co checkout
-git config --global alias.st status
-git config --global alias.oops "commit --amend --no-edit"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.oops "commit --amend --no-edit"
+git config --global alias.pushup "push -u origin HEAD"
+git config --global alias.st status
